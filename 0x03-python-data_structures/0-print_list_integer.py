9b8ce3615deef379e454aab8521bb68b0936ec89
owenmomanyi <owensirtomn@gmail.com>
2022-08-29 11:25:33 -0700

    Prints integers of a list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
+def print_list_integer(my_list=[]):
     for arr in range(len(my_list)):
         if ((idx < 0) or (idx > len(my_list) - 1)):
             return (my_list)
